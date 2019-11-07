0736071653978c49f1f54ab49429dc024d62b726
abautista59 <abautista59@nsmb208-xx.csudh.edu>
2019-11-06 16:39:47 -0800

    You are a bold one

---
+++ Hello.c
@@ -0,0 +1,15 @@
+/*Angel Bauitsta
+ * CSC 251
+ * 11/6/2019
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void main (int)
+{
+
+	puts("Hello There");
+	puts("General Kenobi");
+	return EXIT_SUCCESS;
+}
